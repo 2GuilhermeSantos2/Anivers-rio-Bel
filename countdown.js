0ecd4709cfcaa676c544a5ae1590768c133adb2c
Guilherme Santos <80928105+2GuilhermeSantos2@users.noreply.github.com>
2024-03-09 05:19:24 -0300

    .

--- countdown.js
+++ countdown.js
@@ -4,17 +4,17 @@ const x = setInterval(function() {
   const now = new Date().getTime();
   const distance = countDownDate - now;
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Se a data atual for após a data da contagem regressiva, redirecionar imediatamente
+  if (now > countDownDate) {
+    window.location.href = 'aniversario.html'; // Página de parabéns
+  } else {
+    // Atualizar a contagem regressiva
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-    window.location.href = 'index.html';
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
   }
 }, 1000);
